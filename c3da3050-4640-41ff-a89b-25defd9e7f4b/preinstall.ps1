6ebbe8047b4601446f1adadbc0214db818d8fca0
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:43:08 +0700

    Update preinstall.ps1

--- c3da3050-4640-41ff-a89b-25defd9e7f4b/preinstall.ps1
+++ c3da3050-4640-41ff-a89b-25defd9e7f4b/preinstall.ps1
@@ -1,1 +1,25 @@
-"Preinstall Powershell Executed" | Add-Content -Path c:\local\preinstall.log
+# Functions
+function Write-LogEntry {
+    param(
+        [parameter(Mandatory=$true, HelpMessage="Value added to the RemovedApps.log file.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Value,
+
+        [parameter(Mandatory=$false, HelpMessage="Name of the log file that the entry will written to.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$FileName = "MOTION-Update-PS.log"
+    )
+    # Determine log file location
+    $LogFilePath = Join-Path -Path $env:windir -ChildPath "Temp\$($FileName)"
+
+    # Add value to log file
+    try {
+        Out-File -InputObject $Value -Append -NoClobber -Encoding Default -FilePath $LogFilePath -ErrorAction Stop
+    }
+    catch [System.Exception] {
+        Write-Warning -Message "Unable to append log entry to $($FileName) file"
+    }
+}
+
+Write-LogEntry -Value "preinstall.ps1 Start"
+Write-LogEntry -Value "preinstall.ps1 Complete"
