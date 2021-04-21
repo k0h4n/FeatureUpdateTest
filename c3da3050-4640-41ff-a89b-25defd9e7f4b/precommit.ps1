7bf152bfe91ad456b0dea506bf8d25e89df2f737
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:43:57 +0700

    Update precommit.ps1

--- c3da3050-4640-41ff-a89b-25defd9e7f4b/precommit.ps1
+++ c3da3050-4640-41ff-a89b-25defd9e7f4b/precommit.ps1
@@ -1,1 +1,27 @@
 "precommit.ps1 Powershell Executed" | Add-Content -Path c:\local\precommit.log
+
+# Functions
+function Write-LogEntry {
+    param(
+        [parameter(Mandatory=$true, HelpMessage="Value added to the RemovedApps.log file.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Value,
+
+        [parameter(Mandatory=$false, HelpMessage="Name of the log file that the entry will written to.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$FileName = "MOTION-Update-PS.log"
+    )
+    # Determine log file location
+    $LogFilePath = Join-Path -Path $env:windir -ChildPath "Temp\$($FileName)"
+
+    # Add value to log file
+    try {
+        Out-File -InputObject $Value -Append -NoClobber -Encoding Default -FilePath $LogFilePath -ErrorAction Stop
+    }
+    catch [System.Exception] {
+        Write-Warning -Message "Unable to append log entry to $($FileName) file"
+    }
+}
+
+Write-LogEntry -Value "precommit.ps1 Start"
+Write-LogEntry -Value "precommit.ps1 Complete"
