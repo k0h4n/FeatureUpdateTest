d8c93162e106ae6541b4b28a3ae508bac2340c15
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:39:48 +0700

    Update setupcomplete.cmd

--- UpdateScript/setupcomplete.cmd
+++ UpdateScript/setupcomplete.cmd
@@ -1,10 +1,10 @@
 @ECHO ON
-Echo BEGIN setupcomplete.cmd >> c:\local\setupcomplete.log
+Echo BEGIN setupcomplete.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 c:
 cd /d %~dp0
-cmd /c whoami >> c:\local\setupcomplete.log
+cmd /c whoami >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 START Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "c:\ProgramData\MOTION\script\UpdateScript\setupcomplete.ps1"
 
-Echo END setupcomplete.cmd >> c:\local\setupcomplete.log
+Echo END setupcomplete.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
