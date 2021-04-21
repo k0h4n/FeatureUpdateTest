14837d83358c6a6f3299033003296baa8f344c4a
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:41:52 +0700

    Update success.cmd

--- c3da3050-4640-41ff-a89b-25defd9e7f4b/success.cmd
+++ c3da3050-4640-41ff-a89b-25defd9e7f4b/success.cmd
@@ -1,10 +1,10 @@
 @ECHO ON
-Echo BEGIN success.cmd >> c:\local\success.log
+Echo BEGIN success.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 c:
 cd /d %~dp0
-cmd /c whoami >> c:\local\success.log
+cmd /c whoami >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 START Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "c:\ProgramData\MOTION\script\UpdateScript\success.ps1"
 
-Echo END success.cmd >> c:\local\success.log
+Echo END success.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
