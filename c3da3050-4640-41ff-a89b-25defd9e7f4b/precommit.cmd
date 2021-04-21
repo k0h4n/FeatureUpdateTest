33126f75b98730d9fad419481c5db28e17eeb5ca
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:40:39 +0700

    Update precommit.cmd

--- c3da3050-4640-41ff-a89b-25defd9e7f4b/precommit.cmd
+++ c3da3050-4640-41ff-a89b-25defd9e7f4b/precommit.cmd
@@ -1,10 +1,10 @@
 @ECHO ON
-Echo BEGIN precommit.cmd >> c:\local\precommit.log
+Echo BEGIN precommit.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 c:
 cd /d %~dp0
-cmd /c whoami >> c:\local\precommit.log
+cmd /c whoami >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 START Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "precommit.ps1"
 
-Echo END precommit.cmd >> c:\local\precommit.log
+Echo END precommit.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
