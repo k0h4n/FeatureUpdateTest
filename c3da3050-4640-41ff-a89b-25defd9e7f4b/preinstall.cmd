0280b627c379167092fa47a7f2abcec4f56c3857
k0h4n <k0h4n@users.noreply.github.com>
2021-04-21 13:41:16 +0700

    Update preinstall.cmd

--- c3da3050-4640-41ff-a89b-25defd9e7f4b/preinstall.cmd
+++ c3da3050-4640-41ff-a89b-25defd9e7f4b/preinstall.cmd
@@ -1,10 +1,10 @@
 @ECHO ON
-Echo BEGIN preinstall.cmd >> c:\local\preinstall.log
+Echo BEGIN preinstall.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 c:
 cd /d %~dp0
-cmd /c whoami >> c:\local\preinstall.log
+cmd /c whoami >> %SystemRoot%\Temp\MOTION-Update-CMD.log
 
 START Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "preinstall.ps1"
 
-Echo END preinstall.cmd >> c:\local\preinstall.log
+Echo END preinstall.cmd >> %SystemRoot%\Temp\MOTION-Update-CMD.log
